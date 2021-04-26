eee6958facff599d47f0511eaad3087161030c81
DARKDRAGON532 <69623818+DARKDRAGON532@users.noreply.github.com>
2021-04-27 00:14:56 +0530

    deleted and ignored test.ts

--- test.ts
+++
@@ -1,9 +0,0 @@
-import { Client } from "./src/structures/Client.ts";
-
-const client = new Client({});
-
-const events = client.registerEvent("MESSAGE_CREATE", (message: Object) => {
-    console.log("event registering works")
-});
-
-client.login("ODM2MjY0MDQxODk4MDQ5NTM2.YIbdlA.54xiah6qoI2s7RwUMwpT_FLHuLk").then(r => console.log(r));
