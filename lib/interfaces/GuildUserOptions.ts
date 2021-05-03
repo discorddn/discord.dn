f581317ef4648958371f218c5293c61107a1aa2e
DARKDRAGON532 <69623818+DARKDRAGON532@users.noreply.github.com>
2021-05-03 15:50:09 +0530

    guild user

---
+++ lib/interfaces/GuildUserOptions.ts
@@ -0,0 +1,20 @@
+import Role from "../../src/structures/Role.ts"
+
+export default interface GuildUserOptions {
+    id: string 
+    username: string 
+    tag: string
+    avatar: string 
+    bot: boolean 
+    system?: boolean
+    locale?: string
+    flags?: number
+    nickname: string
+    roles: Array<Role>
+    joinedAt: string
+    boostingSince: string
+    deaf: boolean
+    mute: boolean
+    pending?: boolean
+    permissions?: string
+}
--- src/structures/GuildUser.ts
+++ src/structures/GuildUser.ts
@@ -1,6 +1,27 @@
 import User from "./User.ts"
+import Role from "./Role.ts"
+import { Client } from "./Client.ts"
+import GuildUserOptions from "../../lib/interfaces/GuildUserOptions.ts"
 
 export default class GuildUser extends User {
-    
-    
+    nickname: string
+    roles: Array<Role>
+    joinedAt: string
+    boostingSince: string
+    deaf: boolean
+    mute: boolean
+    pending: boolean | undefined
+    permissions: string | undefined
+
+    constructor(options: GuildUserOptions, client: Client) {
+        super(options, client)
+        this.nickname = options.nickname
+        this.roles = options.roles
+        this.joinedAt = options.joinedAt
+        this.boostingSince = options.boostingSince
+        this.deaf = options.deaf
+        this.mute = options.mute
+        this.pending = options.pending
+        this.permissions = options.permissions
+    }
 }
