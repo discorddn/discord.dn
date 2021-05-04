be996d8117c0688312eaa6d4cd39af7fe00ba299
DARKDRAGON532 <69623818+DARKDRAGON532@users.noreply.github.com>
2021-05-04 17:13:44 +0530

    added some functions on guild user

---
+++ lib/interfaces/GuildUserBanOptions.ts
@@ -0,0 +1,4 @@
+export default interface GuildUserBanOptions {
+    deleteMessageDays?: 0| 1 | 2 | 3 | 4 | 5 | 6 | 7
+    reason?: string
+}
--- src/structures/GuildUser.ts
+++ src/structures/GuildUser.ts
@@ -3,6 +3,7 @@ import Role from "./Role.ts"
 import Guild from "./Guild.ts"
 import { Client } from "./Client.ts"
 import GuildUserOptions from "../../lib/interfaces/GuildUserOptions.ts"
+import GuildUserBanOptions from "../../lib/interfaces/GuildUserBanOptions.ts"
 
 export default class GuildUser extends User {
     client: Client
@@ -28,5 +29,27 @@ export default class GuildUser extends User {
         this.pending = options.pending
         this.permissions = options.permissions
         this.guild = options.guild
+    }
+
+    public setNick(nick: string) {
+        this.client.api.patch(`/guilds/${this.guild.id}/members${this.id}`, {
+            nick
+        })
+        this.nickname = nick
+    }
+
+    public ban(options: GuildUserBanOptions={}) {
+        this.client.api.put(`/guilds/${this.guild.id}/bans/${this.id}`, {
+            delete_message_days: options.deleteMessageDays ?? 0,
+            reason: options.reason ?? ""
+        })
+    }
+
+    public unban() {
+        this.client.api.delete(`/guilds/${this.guild.id}/bans/${this.id}`)
+    }
+
+    public kick() {
+        this.client.api.delete(`/guilds/${this.guild.id}/members/${this.id}`)
     }
 }
